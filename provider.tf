873678a10488ca44e07dc44183f6e3574aaedb2f
Bharath-presidio <138151658+Bharath-presidio@users.noreply.github.com>
2023-07-25 17:51:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "vault" {
-  address = "https://wex-vault-ue1-private-vault-84dfb1f8.eadef17c.z1.hashicorp.cloud:8200"
+  address = "vault_URL"
   namespace    = "admin"
 }
